89fd24f3718f1a44889fe5f11951f72653938081
bryanmsombra27 <bryanjm96@gmail.com>
2025-09-07 20:17:21 -0600

    adding admin layout

---
+++ src/admin/components/AdminTitle.tsx
@@ -0,0 +1,16 @@
+import type { FC } from "react";
+
+interface AdminTitleProps {
+  title: string;
+  subtitle: string;
+}
+const AdminTitle: FC<AdminTitleProps> = ({ subtitle, title }) => {
+  return (
+    <div className="mb-8">
+      <h1 className="text-2xl font-bold text-gray-900 mb-2">{title}</h1>
+      <p className="text-gray-600">{subtitle}</p>
+    </div>
+  );
+};
+
+export default AdminTitle;
--- src/admin/pages/products/AdminProducts.tsx
+++ src/admin/pages/products/AdminProducts.tsx
@@ -1,8 +1,75 @@
+import AdminTitle from "@/admin/components/AdminTitle";
 import type { FC } from "react";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import { Link } from "react-router";
+import { CustomPagination } from "@/components/custom/CustomPagination";
+import { Button } from "@/components/ui/button";
+import { PlusIcon } from "lucide-react";
 
 interface AdminProductsProps {}
 const AdminProducts: FC<AdminProductsProps> = ({}) => {
-  return <h1>Component</h1>;
+  return (
+    <>
+      <div className="flex justify-between  items-center">
+        <AdminTitle
+          title="Productos"
+          subtitle="Aqui puedes ver y administrar todos tus productos"
+        />
+        <div className="flex justify-end mb-10 gap-4">
+          <Link to="/admin/products/new">
+            <Button>
+              <PlusIcon />
+              Nuevo Producto
+            </Button>
+          </Link>
+        </div>
+      </div>
+
+      <Table className="bg-white p-10 shadow-xs border border-gray-200 mb-10">
+        <TableHeader>
+          <TableRow>
+            <TableHead className="w-[100px]">ID</TableHead>
+            <TableHead>Imagen</TableHead>
+            <TableHead>Nombre</TableHead>
+            <TableHead className="text-right">Precio</TableHead>
+            <TableHead>Categoria</TableHead>
+            <TableHead>Inventario</TableHead>
+            <TableHead>Tallas</TableHead>
+            <TableHead>Acciones</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          <TableRow>
+            <TableCell className="font-medium">INV001</TableCell>
+            <TableCell>
+              <img
+                src="https://placehold.co/250*250"
+                alt="koso"
+                className="w-20 h-20 object-cover roounded-md"
+              />
+            </TableCell>
+            <TableCell>Producto 1</TableCell>
+            <TableCell className="text-right">$250.00</TableCell>
+            <TableCell className="text-right">Categoria 1</TableCell>
+            <TableCell className="text-right">30</TableCell>
+            <TableCell className="text-right">XS,S,L</TableCell>
+            <TableCell className="text-right">
+              <Link to={`/admin/products/1`}>Editar</Link>
+            </TableCell>
+          </TableRow>
+        </TableBody>
+      </Table>
+
+      <CustomPagination totalPages={5} />
+    </>
+  );
 };
 
 export default AdminProducts;
